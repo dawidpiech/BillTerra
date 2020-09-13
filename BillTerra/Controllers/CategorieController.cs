e8ee72dc395717500c2801dc271aafd49ad3f530
KamilStepien <stepienkamil97@gmail.com>
2020-09-13 21:31:28 +0200

    merge

---
+++ BillTerra/Controllers/CategorieController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BillTerra.Models;
+using BillTerra.Contexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BillTerra.Controllers
+{
+    public class CategorieController : Controller
+    {
+        private ICategorieRepository categorieRepository;
+        private UserManager<User> userManager;
+
+        public CategorieController(ICategorieRepository categorieRepository, UserManager<User> userManager)
+        {
+            this.categorieRepository = categorieRepository;
+            this.userManager = userManager;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            User user = await userManager.GetUserAsync(HttpContext.User);
+            return View("Categorie", categorieRepository.Categories(user).Result);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Add()
+        {
+            User user = await userManager.GetUserAsync(HttpContext.User);
+            var categorie = new Categorie
+            {
+
+                Name = "Home",
+                User = user
+                
+
+            };
+            await categorieRepository.SaveCategorie(categorie);
+            return RedirectToAction("Index");
+        }
+
+
+
+      
+    }
+}
---
+++ BillTerra/Models/AddDefaultCaterories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillTerra.Models
+{
+    public static class AddDefaultCaterories
+    {
+        public static void Add(ICategorieRepository categorieRepository, User user)
+        {
+            categorieRepository.SaveCategorie(new Categorie { Name = "Dom", User = user });
+            categorieRepository.SaveCategorie(new Categorie { Name = "Wydatki", User = user });
+            categorieRepository.SaveCategorie(new Categorie { Name = "Dochody", User = user });
+        }
+    }
+}
+
