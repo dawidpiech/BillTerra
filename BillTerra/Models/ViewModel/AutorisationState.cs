03707817cfa98f28f0cdba1fd1dd86eecc7d2357
KamilStepien <stepienkamil97@gmail.com>
2020-09-15 20:14:59 +0200

    add AutorisationState model

---
+++ BillTerra/Models/ViewModel/AutorisationState.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillTerra.Models.ViewModel
+{
+    public class AutorisationState
+    {
+
+        public bool CreateAccountSucceeded { get; set; }
+        public IEnumerable<IdentityError> Errors { get; set; }
+    }
+}
